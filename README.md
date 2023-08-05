6d63f482f27a54d7f08be49dcfaf4922f05675d6
liudengfeng <liudengfeng_sd@outlook.com>
2023-08-05 08:42:21 +0800

    llama cpp

--- README.md
+++ README.md
@@ -2,5 +2,21 @@
 
 Immersive speaking practice using LLAMA models
 
+## Llama Model
+
+1. 参考[llama cpp](https://github.com/ggerganov/llama.cpp) Quantized size (4-bit)
+
+
+| Model | Original size | Quantized size (4-bit) |
+| ----: | ------------: | ---------------------: |
+|    7B |         13 GB |                 3.9 GB |
+|   13B |         24 GB |                 7.8 GB |
+|   30B |         60 GB |                19.5 GB |
+|   65B |        120 GB |                38.5 GB |
+
+2. 模型存放目录 `~/llm-models/cpp/`
+   1. `llama-2-7B-chat-ggml-q4_0.bin`
+   2. `llama-2-13B-chat-ggml-q4_0.bin`
+
 ## WSL2
 + [Install the Speech SDK](https://learn.microsoft.com/en-us/azure/ai-services/speech-service/quickstarts/setup-platform?tabs=linux%2Cubuntu)
--- pages/3_🔗_langchain_new.py
+++ pages/3_🔗_langchain_new.py
@@ -1,7 +1,12 @@
 import os
 
 import streamlit as st
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import (
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
 from langchain.callbacks import StreamlitCallbackHandler
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
@@ -10,7 +15,7 @@ from langchain.llms import LlamaCpp
 from langchain.memory import ConversationBufferMemory
 
 user_path = os.path.expanduser("~")
-cpp_model_root = os.path.join(user_path, "llm-models", "llama-2-7b-chat-cpp")
+cpp_model_root = os.path.join(user_path, "llm-models", "cpp")
 cpp_model_names = [f for f in os.listdir(cpp_model_root) if f.endswith(".bin")]
 
 
@@ -31,16 +36,17 @@ cpp_model_name = st.sidebar.selectbox(
     # label_visibility="hidden",
 )
 
-st.sidebar.markdown(
-    "[ℹ️ download model from here](https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML)",
-    unsafe_allow_html=True,
-    help="Make sure save model in `~/llm-models/llama-2-7b-chat-cpp/`",
-)
+# st.sidebar.markdown(
+#     "[ℹ️ download model from here](https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML)",
+#     unsafe_allow_html=True,
+#     help="Make sure save model in `~/llm-models/cpp/`",
+# )
+# st.sidebar.info("download model from here https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML")
 
 model_path = os.path.join(cpp_model_root, cpp_model_name)
 
 # Change this value based on your model and your GPU VRAM pool.
-n_gpu_layers = st.sidebar.slider("n_gpu_layers", min_value=20, max_value=100, value=40)
+n_gpu_layers = st.sidebar.slider("n_gpu_layers", min_value=20, max_value=100, value=128)
 # Should be between 1 and n_ctx, consider the amount of VRAM in your GPU.
 n_batch = st.sidebar.slider("n_batch", min_value=64, max_value=1024, value=512)
 
@@ -51,32 +57,37 @@ st.title("🦜🔗 LangChain: Chat with search")
 callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 
 
-
 # Make sure the model path is correct for your system!
 llm = LlamaCpp(
     model_path=model_path,
     n_gpu_layers=n_gpu_layers,
-    # n_batch=n_batch,
+    n_batch=n_batch,
     top_p=0.9,
+    temperature=0.6,
     callback_manager=callback_manager,
     verbose=True,
 )
 
-prompt_template = ChatPromptTemplate.from_role_strings(
-    [
-        (
-            "system",
-            "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information. Chat history is ```{history}```.",
+# Prompt
+prompt = ChatPromptTemplate(
+    messages=[
+        SystemMessagePromptTemplate.from_template(
+            "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."
         ),
-        ("human", "{user_input}"),
+        # The `variable_name` here is what must align with memory
+        MessagesPlaceholder(variable_name="chat_history"),
+        HumanMessagePromptTemplate.from_template("{question}"),
     ]
 )
-memory = ConversationBufferMemory()
+
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
 conversation = ConversationChain(
     llm=llm,
-    # prompt=prompt_template,
-    # memory=memory,
-    verbose=True
+    prompt=prompt,
+    memory=memory,
+    input_key="question",
+    verbose=True,
 )
 
 if "lc_messages" not in st.session_state:
@@ -92,21 +103,13 @@ for msg in st.session_state.lc_messages:
     st.chat_message(msg["role"], avatar=avatar).write(msg["content"])
 
 
-if prompt := st.chat_input(placeholder="Please input your query here."):
-    st.session_state.lc_messages.append({"role": "user", "content": prompt})
-    with st.chat_message("assistant",avatar="🧑‍🎓"):
-        st.markdown(prompt)
+if question := st.chat_input(placeholder="Please input your query here."):
+    st.session_state.lc_messages.append({"role": "user", "content": question})
+    with st.chat_message("assistant", avatar="🧑‍🎓"):
+        st.markdown(question)
 
-    with st.chat_message("assistant",avatar="🤖"):
+    with st.chat_message("assistant", avatar="🤖"):
         st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
-        response = conversation.predict(user_input=prompt, callbacks=[st_cb])
+        response = conversation.predict(question=question, callbacks=[st_cb])
         st.session_state.lc_messages.append({"role": "assistant", "content": response})
         st.markdown(response)
-
-    # st.write("memory.buffer=",memory.buffer)
-
-# template = """Question: {question}
-
-# Answer: Let's work this out in a step by step way to be sure we have the right answer."""
-
-# prompt = PromptTemplate(template=template, input_variables=["question"])
